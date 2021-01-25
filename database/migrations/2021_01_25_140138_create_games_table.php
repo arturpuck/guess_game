3c493a8770d242a42cfd85a6b4f8af111f46cfd9
Artur <arturmostowiak@gmail.com>
2021-01-25 22:13:32 +0100

    poprawiono_migracje

--- database/migrations/2021_01_25_140138_create_games_table.php
+++ database/migrations/2021_01_25_140138_create_games_table.php
@@ -16,9 +16,12 @@ class CreateGamesTable extends Migration
         Schema::create('games', function (Blueprint $table) {
             $table->id();
             $table->string('player_name')->default('Unnamed player');
-            $table->smallInteger('from')->unsigned()->default(1);
-            $table->smallInteger('to')->unsigned()->default(9);
+            $table->unsignedBigInteger('from')->default(1);
+            $table->unsignedBigInteger('to')->default(9);
+            $table->unsignedBigInteger('number');
             $table->unsignedBigInteger('attempts')->default(3);
+            $table->unsignedBigInteger('used_attempts')->default(0);
+            $table->float('score')->unsigned()->nullable()->default(null);
             $table->enum('game_status', ["pending", "won", "lost"])->default("pending");
             $table->timestamps();
         });
